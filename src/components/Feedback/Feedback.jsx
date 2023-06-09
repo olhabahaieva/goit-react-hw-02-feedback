46404167e80ef61bda8a0abb643f4773f5a4a713
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-09 16:58:42 +0300

    created basic UI for components

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,21 @@
 import { Component } from "react";
+import css from './Feedback.module.css';
 
 class Feedback extends Component {
-   
+   render(){
+    return(
+        <>
+        <div className={css.feedback}>
+            <h1 className={css.heading}>Please leave feedback</h1>
+            <div className={css.buttonContainer}>
+                <button className={css.button}>Good</button>
+                <button className={css.button}>Neutral</button>
+                <button className={css.button}>Bad</button>
+            </div>
+        </div>
+        </>
+    )
+   }
 }
 
 export default Feedback;
