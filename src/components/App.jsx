9b46484f8f4c6f333bc4d83694d6884c00abf75f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-09 21:15:52 +0300

    separate files for feedback and stats

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,37 @@
-import Feedback from "./Feedback";
+import { Component } from 'react';
+import Feedback from './Feedback';
+import Statistics from './Statistics';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Feedback/>
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedbackClick = (buttonName) => {
+    this.setState((prevState) => ({
+      [buttonName]: prevState[buttonName] + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <div>
+          <Feedback onFeedbackClick={this.handleFeedbackClick} />
+          <Statistics feedback={this.state} />
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,37 +2,8 @@ import { Component } from 'react';
 import css from './Feedback.module.css';
 
 class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-  };
-
-  handleClick = e => {
-    const buttonName = e.target.name;
-    this.setState(
-      prevState => ({
-        [buttonName]: prevState[buttonName] + 1,
-      }),
-      () => {
-        this.countTotalFeedback();
-        this.countPositiveFeedbackPercentage();
-      }
-    );
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    this.setState({ total });
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const percent = total > 0 ? (good / total).toFixed(2) * 100 : 0;
-    this.setState({ percent });
+  handleClick = (buttonName) => {
+    this.props.onFeedbackClick(buttonName);
   };
 
   render() {
@@ -44,7 +15,7 @@ class Feedback extends Component {
             <button
               name="good"
               type="submit"
-              onClick={this.handleClick}
+              onClick={() => this.handleClick('good')}
               className={css.button}
             >
               Good
@@ -52,7 +23,7 @@ class Feedback extends Component {
             <button
               name="neutral"
               type="submit"
-              onClick={this.handleClick}
+              onClick={() => this.handleClick('neutral')}
               className={css.button}
             >
               Neutral
@@ -60,20 +31,11 @@ class Feedback extends Component {
             <button
               name="bad"
               type="submit"
-              onClick={this.handleClick}
+              onClick={() => this.handleClick('bad')}
               className={css.button}
             >
               Bad
             </button>
-          </div>
-          <div className={css.stats} />
-          <h2 className={css.heading}>Statistics</h2>
-          <div>
-            <p className={css.text}>Good: {this.state.good}</p>
-            <p className={css.text}>Neutral: {this.state.neutral}</p>
-            <p className={css.text}>Bad: {this.state.bad}</p>
-            <p className={css.text}>Total: {this.state.total}</p>
-            <p className={css.text}>Positive Feedback: {this.state.percent}%</p>
           </div>
         </div>
       </>
